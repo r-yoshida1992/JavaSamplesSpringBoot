454ceb39c1d7565576e87a6997bcf97027b4f6eb
Ren Yoshida <ren.yoshida1992@gmail.com>
2022-11-22 13:21:46 +0900

    initial commit

---
+++ src/main/java/com/example/javasamplesspringboot/JavaSamplesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.javasamplesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSamplesSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSamplesSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javasamplesspringboot/JavaSamplesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javasamplesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSamplesSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
